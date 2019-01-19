182b8bc05201579e2cf68fec0e4459647bcaa834
unknown <abakanosipov@ya.ru>
2019-01-19 17:02:02 +0300

    FirstCase

---
+++ testin.py
@@ -0,0 +1,17 @@
+from selenium import webdriver                           #Импорт вебдрайвера
+from tqdm import tqdm                                    #Импорт прогрессбара
+
+driver = webdriver.Chrome()                              #Инициализация драйвера(браузера)
+
+
+driver.get("https://s3.eu-central-1.amazonaws.com/qa-web-test-task/1.html") #638, 3395 Начальный сайт
+
+#driver.minimize_window()                                #Минимизация окна
+
+for x in tqdm(range(9998)):                              #Цикл до конечного сайта
+    try:                                                 #Если ссылка найдена
+        driver.find_element_by_xpath("/html/body/a").click()
+    except:                                              #Если ссылки нет
+        print(driver.current_url)                        #Печать URL сайта без ссылки
+        driver.get('https://s3.eu-central-1.amazonaws.com/qa-web-test-task/{}.html' .format(x+1)) #Перейти на следущий сайт
+driver.close()
